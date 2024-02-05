cd10269d426b08bb028fd29ff3175edf4f897ceb
Anchal <Anchal2011030@akgec.ac.in>
2024-02-05 13:28:27 +0530

    practising precision

---
+++ mountblue_challenge/practice.cpp
@@ -0,0 +1,84 @@
+// #include <iostream>
+
+// int main() {
+//     int numerator = 7;
+//     float denominator = 2.0;
+
+//     int result = static_cast<int>(numerator / denominator);
+
+//     std::cout << "Result: " << result << std::endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+
+// int main() {
+//     int numerator = 7;
+//     float denominator = 2.0;
+
+//     float result = numerator / denominator;
+
+//     std::cout << "Result: " << result << std::endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+
+// int main() {
+//     int numerator = 7.0;
+//     float denominator = 2;
+
+//     float result = numerator / denominator;
+
+//     std::cout << "Result: " << result << std::endl;
+
+//     return 0;
+// }
+
+#include <iostream>
+
+int main() {
+    int numerator = 7;
+    int denominator = 2;
+
+    float result = static_cast<float>(numerator / denominator);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
+#include <iostream>
+
+int main() {
+    int numerator = 7;
+    int denominator = 2;
+
+    float result = static_cast<float>(numerator) / denominator;
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+// #include <iostream>
+
+// int main() {
+//     float numerator = 7.0;
+//     float denominator = 2.0;
+
+//     float result = numerator / denominator;
+
+//     std::cout << "Result: " << result << std::endl;
+
+//     return 0;
+// }
